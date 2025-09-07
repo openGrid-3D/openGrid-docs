2a3d0a1d961aeda29b48e3ffc620107f9f7b7c71
David Danier <danier@team23.de>
2025-09-07 16:37:08 +0200

    feat(content): ✨ Clarify small details

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://frenchiveruti.github.io/openGrid-docs/',
-	base: '/openGrid-docs',
+    site: 'https://www.opengrid.world',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'openGrid',
